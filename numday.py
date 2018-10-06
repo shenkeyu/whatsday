8179f504fc9980e17d13611748cd224ceedf167f
shenkeyu <fengyangluoyue@163.com>
2018-10-07 04:13:26 +0800

    4.0

--- numday.py
+++ numday.py
@@ -36,14 +36,22 @@ def main():
 
     day_in_month_tup = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
     day_in_month_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day_in_month_dict = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+    days_dict = 0
+    days_dict += input_data_day
+    for j in range(1, input_data_month):
+        days_dict += day_in_month_dict[j]
+
     days = sum(day_in_month_tup[:input_data_month-1])+input_data_day
     if input_data_month > 2 and is_leap_year(input_data_year):
         days += 1
+        days_dict += 1
         day_in_month_list[1] = 29
     days_list = sum(day_in_month_list[:input_data_month-1])+input_data_day
     print('当前日期为此年的第{}天。tup方式'.format(days))
     print('当前日期为此年的第{}天。list方式'.format(days_list))
     print('当前日期为此年的第{}天。set方式'.format(days_set))
+    print('当前日期为此年的第{}天。dict方式'.format(days_dict))
 
 if __name__ == '__main__':
     main()
